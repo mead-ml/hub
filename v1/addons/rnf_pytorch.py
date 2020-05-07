920e39a60cf3fa19b1b828d328f1e8606486c141
dpressel <dpressel@gmail.com>
2020-05-07 00:03:23 -0400

    simplify

--- v1/addons/rnf_pytorch.py
+++ v1/addons/rnf_pytorch.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from baseline.pytorch.classify import ClassifierModelBase
+from typing import Tuple
+from baseline.pytorch.classify import EmbedPoolStackClassifier
 from baseline.model import register_model
+from eight_mile.pytorch.layers import WithDropout
 
 def ngrams(x, filtsz, mxlen):
     chunks = []
@@ -12,32 +14,33 @@ def ngrams(x, filtsz, mxlen):
     chunks = torch.stack(chunks, 1)
     return chunks
 
-
-@register_model(task='classify', name='rnf')
-class RNFWordClassifier(ClassifierModelBase):
-
-    def __init__(self):
-        super(RNFWordClassifier, self).__init__()
-
-    def init_pool(self, dsz, **kwargs):
+class RNF(nn.Module):
+    def __init__(self, input_dim: int, **kwargs):
+        super().__init__()
         self.filtsz = kwargs['filtsz']
         self.mxlen = kwargs.get('mxlen', 100)
         pdrop = kwargs.get('dropout', 0.4)
-        self.dropout_pre = nn.Dropout(pdrop)
-        rnnsz = kwargs.get('rnnsz', 300)
-        self.rnf = nn.LSTM(dsz, rnnsz, batch_first=True)
-        self.pool_dropout = nn.Dropout(kwargs.get('pool_dropout', 0.))
-        return rnnsz
+        self.output_dim = kwargs.get('rnnsz', 300)
+        self.rnf = nn.LSTM(input_dim, self.output_dim, batch_first=True)
+        self.dropout = nn.Dropout(pdrop)
 
-    def pool(self, btc, lengths):
-
-        btc = self.dropout_pre(btc)
-        btfc = ngrams(btc, self.filtsz, self.mxlen)
+    def forward(self, inputs: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        btc, lengths = inputs
+        btc = self.dropout(btc)
+        mxlen = max(torch.max(lengths), self.filtsz+1)
+        btfc = ngrams(btc, self.filtsz, mxlen)
         B, T, F, C = btfc.shape
         btc = btfc.view(B*T, F, C)
         output, hidden = self.rnf(btc)
-        hidden = hidden[0].view(hidden[0].shape[1:])
+        hidden = self.dropout(hidden[0])
+        hidden = hidden.view(hidden.shape[1:])
         btc = hidden.view(B, T, -1)
         bc = btc.max(1)[0]
-        return self.pool_dropout(bc)
+        return bc
+
+@register_model(task='classify', name='rnf')
+class RNFWordClassifier(EmbedPoolStackClassifier):
+
+    def init_pool(self, input_dim: int, **kwargs) -> nn.Module:
+        return WithDropout(RNF(input_dim, **kwargs), kwargs.get('pool_dropout', 0.0))
 
